e086e5a3de1beba39c22b601d8b16db3d62d88de
Coda Hale <coda.hale@gmail.com>
2009-12-04 11:34:22 -0800

    Added IntegerEquivalent.

---
+++ src/main/java/com/wesabe/grendel/util/IntegerEquivalent.java
@@ -0,0 +1,15 @@
+package com.wesabe.grendel.util;
+
+/**
+ * A utility interface for describing objects which have an integer value
+ * associated with them.
+ * 
+ * @author coda
+ *
+ */
+public interface IntegerEquivalent {
+	/**
+	 * Returns the object as an integer.
+	 */
+	public abstract int toInteger();
+}
