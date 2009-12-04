650064ac58920fa0d0eef590208d1f57fd473460
Coda Hale <coda.hale@gmail.com>
2009-12-04 11:40:48 -0800

    Added CompressionAlgorithm.

---
+++ src/main/java/com/wesabe/grendel/openpgp/CompressionAlgorithm.java
@@ -0,0 +1,66 @@
+package com.wesabe.grendel.openpgp;
+
+import org.bouncycastle.bcpg.CompressionAlgorithmTags;
+
+import com.wesabe.grendel.util.IntegerEquivalent;
+
+/**
+ * A compression algorithm for OpenPGP messages.
+ * 
+ * @author coda
+ * @see <a href="http://www.ietf.org/rfc/rfc4880.txt">Section 9.3, RFC 4880</a>
+ */
+public enum CompressionAlgorithm implements IntegerEquivalent {
+	/**
+	 * Uncompressed
+	 */
+	NONE(	"None",		CompressionAlgorithmTags.UNCOMPRESSED),
+	
+	/**
+	 * ZLIB
+	 * 
+	 * @see <a href="http://www.ietf.org/rfc/rfc1951.txt">RFC 1951</a>
+	 */
+	ZLIB(	"ZLIB",		CompressionAlgorithmTags.ZLIB),
+	
+	/**
+	 * ZIP
+	 * 
+	 * @see <a href="http://www.ietf.org/rfc/rfc1950.txt">RFC 1950</a>
+	 */
+	ZIP(	"ZIP",		CompressionAlgorithmTags.ZIP),
+	
+	/**
+	 * BZip2
+	 * 
+	 * @see <a href="http://www.bzip.org/">bzip.org</a>
+	 */
+	BZIP2(	"BZIP2",	CompressionAlgorithmTags.BZIP2);
+	
+	/**
+	 * The default compression algorithm to use.
+	 */
+	public static final CompressionAlgorithm DEFAULT = BZIP2;
+	
+	private final String name;
+	private final int value;
+	
+	private CompressionAlgorithm(String name, int value) {
+		this.name = name;
+		this.value = value;
+	}
+	
+	/**
+	 * Returns the equivalent value of {@link CompressionAlgorithmTags}.
+	 * 
+	 */
+	@Override
+	public int toInteger() {
+		return value;
+	}
+	
+	@Override
+	public String toString() {
+		return name;
+	}
+}
---
+++ src/test/java/com/wesabe/grendel/openpgp/tests/CompressionAlgorithmTest.java
@@ -0,0 +1,68 @@
+package com.wesabe.grendel.openpgp.tests;
+
+import static org.fest.assertions.Assertions.*;
+
+import org.bouncycastle.bcpg.CompressionAlgorithmTags;
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+import com.wesabe.grendel.openpgp.CompressionAlgorithm;
+
+@RunWith(Enclosed.class)
+public class CompressionAlgorithmTest {
+	public static class None {
+		@Test
+		public void itHasTheSameValueAsTheBCTag() throws Exception {
+			assertThat(CompressionAlgorithm.NONE.toInteger()).isEqualTo(CompressionAlgorithmTags.UNCOMPRESSED);
+		}
+		
+		@Test
+		public void itIsHumanReadable() throws Exception {
+			assertThat(CompressionAlgorithm.NONE.toString()).isEqualTo("None");
+		}
+	}
+	
+	public static class ZLIB {
+		@Test
+		public void itHasTheSameValueAsTheBCTag() throws Exception {
+			assertThat(CompressionAlgorithm.ZLIB.toInteger()).isEqualTo(CompressionAlgorithmTags.ZLIB);
+		}
+		
+		@Test
+		public void itIsHumanReadable() throws Exception {
+			assertThat(CompressionAlgorithm.ZLIB.toString()).isEqualTo("ZLIB");
+		}
+	}
+	
+	public static class ZIP {
+		@Test
+		public void itHasTheSameValueAsTheBCTag() throws Exception {
+			assertThat(CompressionAlgorithm.ZIP.toInteger()).isEqualTo(CompressionAlgorithmTags.ZIP);
+		}
+		
+		@Test
+		public void itIsHumanReadable() throws Exception {
+			assertThat(CompressionAlgorithm.ZIP.toString()).isEqualTo("ZIP");
+		}
+	}
+	
+	public static class BZIP2 {
+		@Test
+		public void itHasTheSameValueAsTheBCTag() throws Exception {
+			assertThat(CompressionAlgorithm.BZIP2.toInteger()).isEqualTo(CompressionAlgorithmTags.BZIP2);
+		}
+		
+		@Test
+		public void itIsHumanReadable() throws Exception {
+			assertThat(CompressionAlgorithm.BZIP2.toString()).isEqualTo("BZIP2");
+		}
+	}
+	
+	public static class Default {
+		@Test
+		public void itUsesBZIP2ByDefault() throws Exception {
+			assertThat(CompressionAlgorithm.DEFAULT).isEqualTo(CompressionAlgorithm.BZIP2);
+		}
+	}
+}
