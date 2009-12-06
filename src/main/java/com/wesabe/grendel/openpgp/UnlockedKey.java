8debe7ecd9f98149921c352d629307173254d80a
Coda Hale <coda.hale@gmail.com>
2009-12-06 12:21:14 -0800

    Added documentation to UnlockedKey.

--- src/main/java/com/wesabe/grendel/openpgp/UnlockedKey.java
+++ src/main/java/com/wesabe/grendel/openpgp/UnlockedKey.java
@@ -2,6 +2,14 @@ package com.wesabe.grendel.openpgp;
 
 import org.bouncycastle.openpgp.PGPPrivateKey;
 
+/**
+ * A PGP key which has been unlocked by its passphrase.
+ * 
+ * @author coda
+ */
 public interface UnlockedKey {
+	/**
+	 * Returns the key's private key.
+	 */
 	/* default */ abstract PGPPrivateKey getPrivateKey();
 }
