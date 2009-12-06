40e3e1a84b54f6774f6b4807203b55dab2c3cd7f
Coda Hale <coda.hale@gmail.com>
2009-12-06 12:20:52 -0800

    Added documentation to SubKey.

--- src/main/java/com/wesabe/grendel/openpgp/SubKey.java
+++ src/main/java/com/wesabe/grendel/openpgp/SubKey.java
@@ -7,15 +7,30 @@ import org.bouncycastle.openpgp.PGPException;
 import org.bouncycastle.openpgp.PGPPrivateKey;
 import org.bouncycastle.openpgp.PGPSecretKey;
 
+/**
+ * A PGP subkey, used for encrypting and decrypting data. <b>Must</b> be
+ * certified by a {@link MasterKey}.
+ * 
+ * @author coda
+ */
 public class SubKey extends AbstractKey {
 	private final MasterKey masterKey;
 	
+	/**
+	 * Loads a subkey from a {@link PGPSecretKey} instance and verifies the
+	 * certification by the given {@link MasterKey}.
+	 * 
+	 * @param key a {@link PGPSecretKey} instance
+	 * @param masterKey the signing {@link MasterKey}
+	 * @return a {@link SubKey} instance
+	 * @throws CryptographicException if {@code masterKey}'s certification is invalid
+	 */
 	public static SubKey load(PGPSecretKey key, MasterKey masterKey) throws CryptographicException {
 		final SubKey subKey = new SubKey(key, masterKey);
 		if (verify(subKey, masterKey)) {
 			return subKey;
 		}
-		
+
 		throw new CryptographicException("not a valid subkey");
 	}
 	
@@ -53,6 +68,9 @@ public class SubKey extends AbstractKey {
 		return masterKey.getPreferredSymmetricAlgorithms();
 	}
 	
+	/**
+	 * Returns the paired {@link MasterKey}.
+	 */
 	public MasterKey getMasterKey() {
 		return masterKey;
 	}
