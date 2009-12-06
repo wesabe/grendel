214af3c558174c0af494c2183dc89f9e02309d2d
Coda Hale <coda.hale@gmail.com>
2009-12-06 12:21:29 -0800

    Added documentation to UnlockedKeySet.

--- src/main/java/com/wesabe/grendel/openpgp/UnlockedKeySet.java
+++ src/main/java/com/wesabe/grendel/openpgp/UnlockedKeySet.java
@@ -1,15 +1,26 @@
 package com.wesabe.grendel.openpgp;
 
+/**
+ * An unlocked {@link KeySet}.
+ * 
+ * @author coda
+ */
 public class UnlockedKeySet extends KeySet {
 	
 	protected UnlockedKeySet(UnlockedMasterKey masterKey, UnlockedSubKey subKey) {
 		super(masterKey, subKey);
 	}
 	
+	/**
+	 * Returns the {@link UnlockedMasterKey}.
+	 */
 	public UnlockedMasterKey getUnlockedMasterKey() {
 		return (UnlockedMasterKey) getMasterKey();
 	}
 	
+	/**
+	 * Returns the {@link UnlockedSubKey}.
+	 */
 	public UnlockedSubKey getUnlockedSubKey() {
 		return (UnlockedSubKey) getSubKey();
 	}
