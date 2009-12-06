1f54e9522613c242294e1ec2541f61ee8c2953f2
Coda Hale <coda.hale@gmail.com>
2009-12-06 12:17:28 -0800

    Added documentation to KeySetGenerator.
    Also refactored out the anonymous Callable into its own private,
    static class for performance and clarity.

--- src/main/java/com/wesabe/grendel/openpgp/KeySetGenerator.java
+++ src/main/java/com/wesabe/grendel/openpgp/KeySetGenerator.java
@@ -1,8 +1,8 @@
 package com.wesabe.grendel.openpgp;
 
+import java.security.GeneralSecurityException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
-import java.security.NoSuchProviderException;
 import java.security.SecureRandom;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
@@ -22,40 +22,107 @@ import org.joda.time.DateTime;
 import com.google.inject.internal.ImmutableList;
 import com.wesabe.grendel.util.IntegerEquivalents;
 
+/**
+ * A multithreaded generator for {@link KeySet}s.
+ * 
+ * Generates master keys using {@link AsymmetricAlgorithm#ENCRYPTION_DEFAULT},
+ * and subkeys using {@link AsymmetricAlgorithm#SIGNING_DEFAULT}.
+ * 
+ * @author coda
+ */
 public class KeySetGenerator {
+	
+	/**
+	 * A {@link Callable} which returns a new key pair.
+	 * @author coda
+	 *
+	 */
+	private static class GeneratorTask implements Callable<KeyPair> {
+		private final AsymmetricAlgorithm algorithm;
+		private final SecureRandom random;
+		
+		public GeneratorTask(AsymmetricAlgorithm algorithm, SecureRandom random) {
+			this.algorithm = algorithm;
+			this.random = random;
+		}
+		
+		@Override
+		public KeyPair call() throws Exception {
+			final KeyPairGenerator generator = KeyPairGenerator.getInstance(algorithm.getName(), "BC");
+			generator.initialize(algorithm.getAlgorithmParameterSpec(), random);
+			return generator.generateKeyPair();
+		}
+	}
+	
 	private final SecureRandom random;
 	private final ExecutorService executor;
-
+	
+	/**
+	 * Creates a new {@link KeySetGenerator}.
+	 * 
+	 * @param random a secure random number generator
+	 */
 	public KeySetGenerator(SecureRandom random) {
 		this.random = random;
 		this.executor = Executors.newCachedThreadPool();
 	}
+	
+	/**
+	 * Generates a new {@link KeySet}.
+	 * 
+	 * @param userId the user ID, in {@code First Last <email@example.com>} format
+	 * @param passphrase the user's passphrase
+	 * @return a keyset for the user
+	 * @throws CryptographicException if there was an error generating the keyset
+	 */
+	public KeySet generate(String userId, char[] passphrase) throws CryptographicException {
+		try {
+			final Future<KeyPair> masterKeyPair = generateKeyPair(
+				AsymmetricAlgorithm.SIGNING_DEFAULT
+			);
+			final Future<KeyPair> subKeyPair = generateKeyPair(
+				AsymmetricAlgorithm.ENCRYPTION_DEFAULT
+			);
 
-	public KeySet generate(String userId, char[] passphrase) throws ExecutionException, InterruptedException, NoSuchProviderException, PGPException, CryptographicException {
-		final Future<KeyPair> masterKeyPairFuture = generateKeyPair(AsymmetricAlgorithm.SIGNING_DEFAULT);
-		final Future<KeyPair> subKeyPairFuture = generateKeyPair(AsymmetricAlgorithm.ENCRYPTION_DEFAULT);
+			final PGPKeyPair masterPGPKeyPair = new PGPKeyPair(
+				AsymmetricAlgorithm.SIGNING_DEFAULT.toInteger(),
+				masterKeyPair.get(),
+				new DateTime().toDate()
+			);
 
-		final PGPKeyPair masterPGPKeyPair = new PGPKeyPair(AsymmetricAlgorithm.SIGNING_DEFAULT.toInteger(), masterKeyPairFuture.get(), new DateTime().toDate());
+			final PGPKeyRingGenerator generator = new PGPKeyRingGenerator(
+				SignatureType.POSITIVE_CERTIFICATION.toInteger(),
+				masterPGPKeyPair,
+				userId,
+				SymmetricAlgorithm.DEFAULT.toInteger(),
+				passphrase,
+				true, // use SHA-1 instead of MD5
+				generateMasterKeySettings(),
+				null, // don't store any key settings unhashed
+				random,
+				BouncyCastleProvider.PROVIDER_NAME
+			);
 
-		final PGPKeyRingGenerator generator = new PGPKeyRingGenerator(
-			SignatureType.POSITIVE_CERTIFICATION.toInteger(),
-			masterPGPKeyPair,
-			userId,
-			SymmetricAlgorithm.DEFAULT.toInteger(),
-			passphrase,
-			true, // use SHA-1 instead of MD5
-			generateMasterKeySettings(),
-			null, // don't store any key settings unhashed
-			random,
-			BouncyCastleProvider.PROVIDER_NAME
-		);
+			final PGPKeyPair subPGPKeyPair = new PGPKeyPair(
+				AsymmetricAlgorithm.ENCRYPTION_DEFAULT.toInteger(),
+				subKeyPair.get(),
+				new DateTime().toDate()
+			);
 
-		final PGPKeyPair subPGPKeyPair = new PGPKeyPair(AsymmetricAlgorithm.ENCRYPTION_DEFAULT.toInteger(), subKeyPairFuture.get(), new DateTime().toDate());
+			generator.addSubKey(subPGPKeyPair, generateSubKeySettings(), null);
 
-		generator.addSubKey(subPGPKeyPair, generateSubKeySettings(), null);
-
-		final PGPSecretKeyRing keyRing = generator.generateSecretKeyRing();
-		return KeySet.load(keyRing);
+			final PGPSecretKeyRing keyRing = generator.generateSecretKeyRing();
+			return KeySet.load(keyRing);
+			
+		} catch (GeneralSecurityException e) {
+			throw new CryptographicException(e);
+		} catch (PGPException e) {
+			throw new CryptographicException(e);
+		} catch (InterruptedException e) {
+			throw new CryptographicException(e);
+		} catch (ExecutionException e) {
+			throw new CryptographicException(e);
+		}
 	}
 
 	private PGPSignatureSubpacketVector generateSubKeySettings() {
@@ -66,9 +133,15 @@ public class KeySetGenerator {
 
 	private PGPSignatureSubpacketVector generateMasterKeySettings() {
 		final PGPSignatureSubpacketGenerator settings = new PGPSignatureSubpacketGenerator();
-		settings.setKeyFlags(false, IntegerEquivalents.toBitmask(KeyFlag.MASTER_KEY_DEFAULTS));
-		settings.setPreferredSymmetricAlgorithms(false, IntegerEquivalents.toIntArray(SymmetricAlgorithm.ACCEPTABLE_ALGORITHMS));
-		settings.setPreferredHashAlgorithms(false, IntegerEquivalents.toIntArray(HashAlgorithm.ACCEPTABLE_ALGORITHMS));
+		settings.setKeyFlags(false,
+			IntegerEquivalents.toBitmask(KeyFlag.MASTER_KEY_DEFAULTS)
+		);
+		settings.setPreferredSymmetricAlgorithms(false,
+			IntegerEquivalents.toIntArray(SymmetricAlgorithm.ACCEPTABLE_ALGORITHMS)
+		);
+		settings.setPreferredHashAlgorithms(false,
+			IntegerEquivalents.toIntArray(HashAlgorithm.ACCEPTABLE_ALGORITHMS)
+		);
 		settings.setPreferredCompressionAlgorithms(false,
 				IntegerEquivalents.toIntArray(
 					ImmutableList.of(
@@ -82,13 +155,6 @@ public class KeySetGenerator {
 	}
 
 	private Future<KeyPair> generateKeyPair(final AsymmetricAlgorithm algorithm) {
-		return executor.submit(new Callable<KeyPair>() {
-			@Override
-			public KeyPair call() throws Exception {
-				final KeyPairGenerator generator = KeyPairGenerator.getInstance(algorithm.getName(), "BC");
-				generator.initialize(algorithm.getAlgorithmParameterSpec(), random);
-				return generator.generateKeyPair();
-			}
-		});
+		return executor.submit(new GeneratorTask(algorithm, random));
 	}
 }
