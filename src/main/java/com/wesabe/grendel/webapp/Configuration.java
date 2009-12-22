9571dcd2adb0ca25ad34ff5663e5f50a4b70c120
Coda Hale <coda.hale@gmail.com>
2009-12-22 13:50:05 -0800

    Added latency logging to request log.

--- src/main/java/com/wesabe/grendel/webapp/Configuration.java
+++ src/main/java/com/wesabe/grendel/webapp/Configuration.java
@@ -2,6 +2,9 @@ package com.wesabe.grendel.webapp;
 
 import java.security.SecureRandom;
 
+import org.eclipse.jetty.server.NCSARequestLog;
+import org.eclipse.jetty.server.RequestLog;
+
 import com.codahale.shore.AbstractConfiguration;
 import com.google.inject.AbstractModule;
 import com.google.inject.Stage;
@@ -20,6 +23,13 @@ public class Configuration extends AbstractConfiguration {
 			}
 		});
 		setStage(Stage.PRODUCTION);
+	}
+	
+	@Override
+	protected void configureRequestLog(RequestLog log) {
+		final NCSARequestLog ncsaLog = (NCSARequestLog) log;
+		ncsaLog.setExtended(true);
+		ncsaLog.setLogLatency(true);
 	}
 
 	@Override
