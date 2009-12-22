f487b4b7419ef08c3d453493144b01a947c80936
Coda Hale <coda.hale@gmail.com>
2009-12-22 13:05:11 -0800

    Added Validatable.

---
+++ src/main/java/com/wesabe/grendel/resources/dto/Validatable.java
@@ -0,0 +1,15 @@
+package com.wesabe.grendel.resources.dto;
+
+/**
+ * An interface for data transfer objects (DTOs) capable of validation.
+ * 
+ * @author coda
+ */
+public interface Validatable {
+	/**
+	 * Validates the object's state, throwing an exception if invalid.
+	 * 
+	 * @throws ValidationException if the object has an invalid state
+	 */
+	public abstract void validate() throws ValidationException;
+}
