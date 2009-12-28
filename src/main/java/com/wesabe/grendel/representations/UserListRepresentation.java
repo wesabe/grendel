be918cd1bffc3dd17b21b9a1dd156f598cb9b703
Coda Hale <coda.hale@gmail.com>
2009-12-27 16:46:57 -0800

    Added UserListRepresentation.

---
+++ src/main/java/com/wesabe/grendel/representations/UserListRepresentation.java
@@ -0,0 +1,58 @@
+package com.wesabe.grendel.representations;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.ws.rs.core.UriInfo;
+
+import org.codehaus.jackson.annotate.JsonGetter;
+
+import com.google.common.collect.Lists;
+import com.wesabe.grendel.entities.User;
+import com.wesabe.grendel.resources.UsersResource;
+
+public class UserListRepresentation {
+	public static class UserListItem {
+		private final String id;
+		private final URI uri;
+		
+		public UserListItem(String id, URI uri) {
+			this.id = id;
+			this.uri = uri;
+		}
+		
+		@JsonGetter("id")
+		public String getId() {
+			return id;
+		}
+		
+		@JsonGetter("uri")
+		public String getUri() {
+			return uri.toASCIIString();
+		}
+	}
+	
+	private final UriInfo uriInfo;
+	private final List<User> users;
+	
+	public UserListRepresentation(UriInfo uriInfo, List<User> users) {
+		this.uriInfo = uriInfo;
+		this.users = users;
+	}
+	
+	@JsonGetter("users")
+	public List<UserListItem> getUsers() {
+		final List<UserListItem> items = Lists.newArrayListWithExpectedSize(users.size());
+		for (User user : users) {
+			items.add(new UserListItem(
+				user.getId(),
+				// FIXME coda@wesabe.com -- Dec 27, 2009: direct this to where it should go
+				uriInfo.getBaseUriBuilder()
+							.path(UsersResource.class)
+							.build(user.getId())
+			));
+		}
+		return items;
+	}
+
+}
---
+++ src/test/java/com/wesabe/grendel/representations/tests/UserListRepresentationTest.java
@@ -0,0 +1,61 @@
+package com.wesabe.grendel.representations.tests;
+
+import static org.fest.assertions.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import com.google.common.collect.ImmutableList;
+import com.wesabe.grendel.entities.User;
+import com.wesabe.grendel.representations.UserListRepresentation;
+
+@RunWith(Enclosed.class)
+public class UserListRepresentationTest {
+	public static class Serializing_A_User_List {
+		private UriInfo uriInfo;
+		private UserListRepresentation rep;
+		private User user;
+		
+		@Before
+		public void setup() throws Exception {
+			this.uriInfo = mock(UriInfo.class);
+			when(uriInfo.getBaseUriBuilder()).thenAnswer(new Answer<UriBuilder>() {
+				@Override
+				public UriBuilder answer(InvocationOnMock invocation) throws Throwable {
+					return UriBuilder.fromUri("http://example.com");
+				}
+			});
+			
+			this.user = mock(User.class);
+			when(user.getId()).thenReturn("mrpeepers");
+			
+			this.rep = new UserListRepresentation(uriInfo, ImmutableList.of(user));
+		}
+		
+		@Test
+		public void itSerializesIntoJSON() throws Exception {
+			final ObjectMapper mapper = new ObjectMapper();
+			
+			assertThat(mapper.writeValueAsString(rep)).isEqualTo(
+				"{" +
+					"\"users\":[" +
+						"{" +
+							"\"id\":\"mrpeepers\"," +
+							// FIXME coda@wesabe.com -- Dec 27, 2009: direct this to where it should go
+							"\"uri\":\"http://example.com/users/\"" +
+						"}" +
+					"]" +
+				"}"
+			);
+		}
+	}
+}
