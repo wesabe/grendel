1a2547fc9b6a9d83ab57c6269968ef724c4050c0
Coda Hale <coda.hale@gmail.com>
2010-01-02 00:38:50 -0800

    Removed unused import from DocumentResource.

--- src/main/java/com/wesabe/grendel/resources/DocumentResource.java
+++ src/main/java/com/wesabe/grendel/resources/DocumentResource.java
@@ -23,7 +23,6 @@ import com.google.inject.Provider;
 import com.wesabe.grendel.auth.Credentials;
 import com.wesabe.grendel.auth.Session;
 import com.wesabe.grendel.entities.Document;
-import com.wesabe.grendel.entities.User;
 import com.wesabe.grendel.entities.dao.DocumentDAO;
 import com.wesabe.grendel.entities.dao.UserDAO;
 import com.wesabe.grendel.openpgp.CryptographicException;
