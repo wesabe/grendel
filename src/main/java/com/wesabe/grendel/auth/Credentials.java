b26c8c5398efbeb45ded6489a048b7b48f406a90
Coda Hale <coda.hale@gmail.com>
2010-01-01 23:41:33 -0800

    Added Credentials#buildSession().

--- src/main/java/com/wesabe/grendel/auth/Credentials.java
+++ src/main/java/com/wesabe/grendel/auth/Credentials.java
@@ -1,13 +1,20 @@
 package com.wesabe.grendel.auth;
 
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+
+import com.wesabe.grendel.entities.User;
+import com.wesabe.grendel.entities.dao.UserDAO;
+import com.wesabe.grendel.openpgp.CryptographicException;
+import com.wesabe.grendel.openpgp.UnlockedKeySet;
 
 /**
  * A set of Basic authentication credentials.
  *
  * @see BasicAuthProvider
+ * @see Session
  * @author coda
  */
 public class Credentials {
@@ -46,5 +53,51 @@ public class Credentials {
 	 */
 	public String getPassword() {
 		return password;
+	}
+	
+	/**
+	 * Given a {@link UserDAO}, finds the associated {@link User} and returns a
+	 * {@link Session}.
+	 * 
+	 * @param userDAO
+	 *            a {@link UserDAO}
+	 * @throws WebApplicationException
+	 *             if the user can't be found, or if the user's password is
+	 *             incorrect
+	 */
+	public Session buildSession(UserDAO userDAO) throws WebApplicationException {
+		final User user = userDAO.findById(username);
+		if (user != null) {
+			try {
+				final UnlockedKeySet keySet = user.getKeySet().unlock(password.toCharArray());
+				return new Session(user, keySet);
+			} catch (CryptographicException e) {
+				throw new WebApplicationException(CHALLENGE);
+			}
+		}
+		
+		throw new WebApplicationException(CHALLENGE);
+	}
+	
+	/**
+	 * Given a {@link UserDAO} and an allowed {@link User} id, finds the
+	 * associated {@link User} and returns a {@link Session}.
+	 * 
+	 * @param userDAO
+	 *            a {@link UserDAO}
+	 * @param allowedId
+	 *            the id of the only {@link User} which should be allowed access
+	 *            to session context
+	 * @throws WebApplicationException
+	 *             if the user can't be found, or if the user's password is
+	 *             incorrect
+	 */
+	public Session buildSession(UserDAO userDAO, String allowedId) {
+		final Session session = buildSession(userDAO);
+		if (session.getUser().getId().equals(allowedId)) {
+			return session;
+		}
+		
+		throw new WebApplicationException(Status.FORBIDDEN);
 	}
 }
--- src/test/java/com/wesabe/grendel/auth/tests/CredentialsTest.java
+++ src/test/java/com/wesabe/grendel/auth/tests/CredentialsTest.java
@@ -1,7 +1,10 @@
 package com.wesabe.grendel.auth.tests;
 
 import static org.fest.assertions.Assertions.*;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response.Status;
 
@@ -11,6 +14,12 @@ import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 
 import com.wesabe.grendel.auth.Credentials;
+import com.wesabe.grendel.auth.Session;
+import com.wesabe.grendel.entities.User;
+import com.wesabe.grendel.entities.dao.UserDAO;
+import com.wesabe.grendel.openpgp.CryptographicException;
+import com.wesabe.grendel.openpgp.KeySet;
+import com.wesabe.grendel.openpgp.UnlockedKeySet;
 
 @RunWith(Enclosed.class)
 public class CredentialsTest {
@@ -30,6 +39,139 @@ public class CredentialsTest {
 		@Test
 		public void itHasAPassword() throws Exception {
 			assertThat(creds.getPassword()).isEqualTo("hah");
+		}
+	}
+	
+	private static abstract class Session_Context {
+		protected Credentials creds;
+		protected UserDAO userDAO;
+		protected KeySet keySet;
+		protected UnlockedKeySet unlockedKeySet;
+		protected User user;
+
+		public void setup() throws Exception {
+			this.unlockedKeySet = mock(UnlockedKeySet.class);
+			
+			this.keySet = mock(KeySet.class);
+			when(keySet.unlock(any(char[].class))).thenReturn(unlockedKeySet);
+			
+			this.user = mock(User.class);
+			when(user.getId()).thenReturn("woo");
+			when(user.getKeySet()).thenReturn(keySet);
+			
+			this.userDAO = mock(UserDAO.class);
+			when(userDAO.findById("woo")).thenReturn(user);
+			
+			this.creds = new Credentials("woo", "hah");
+		}
+		
+	}
+	
+	public static class Building_A_Session_For_A_Nonexistent_User extends Session_Context {
+		@Before
+		@Override
+		public void setup() throws Exception {
+			super.setup();
+			
+			when(userDAO.findById("woo")).thenReturn(null);
+		}
+		
+		@Test
+		public void itThrowsAnAuthChallenge() throws Exception {
+			try {
+				creds.buildSession(userDAO);
+				fail("should have thrown an auth challenge, but didn't");
+			} catch (WebApplicationException e) {
+				assertThat(e.getResponse()).isEqualTo(Credentials.CHALLENGE);
+			}
+		}
+	}
+	
+	public static class Building_A_Session_For_A_Bad_Password extends Session_Context {
+		@Before
+		@Override
+		public void setup() throws Exception {
+			super.setup();
+			
+			when(keySet.unlock(any(char[].class))).thenThrow(new CryptographicException("augh"));
+		}
+		
+		@Test
+		public void itAttemptsToUnlockTheKeySet() throws Exception {
+			try {
+				creds.buildSession(userDAO);
+			} catch (WebApplicationException e) {}
+			
+			verify(keySet).unlock("hah".toCharArray());
+		}
+		
+		@Test
+		public void itThrowsAnAuthChallenge() throws Exception {
+			try {
+				creds.buildSession(userDAO);
+				fail("should have thrown an auth challenge, but didn't");
+			} catch (WebApplicationException e) {
+				assertThat(e.getResponse()).isEqualTo(Credentials.CHALLENGE);
+			}
+		}
+	}
+	
+	public static class Building_A_Session_For_Valid_Creds extends Session_Context {
+		@Before
+		@Override
+		public void setup() throws Exception {
+			super.setup();
+		}
+		
+		@Test
+		public void itAttemptsToUnlockTheKeySet() throws Exception {
+			try {
+				creds.buildSession(userDAO);
+			} catch (WebApplicationException e) {}
+			
+			verify(keySet).unlock("hah".toCharArray());
+		}
+		
+		@Test
+		public void itReturnsASessionWithTheUserAndKeySet() throws Exception {
+			final Session session = creds.buildSession(userDAO);
+			
+			assertThat(session.getUser()).isEqualTo(user);
+			assertThat(session.getKeySet()).isEqualTo(unlockedKeySet);
+		}
+	}
+	
+	public static class Building_A_Session_For_Valid_Creds_With_An_Expected_id extends Session_Context {
+		@Before
+		@Override
+		public void setup() throws Exception {
+			super.setup();
+		}
+		
+		@Test
+		public void itReturnsASessionWithTheUserAndKeySet() throws Exception {
+			final Session session = creds.buildSession(userDAO, "woo");
+			
+			assertThat(session.getUser()).isEqualTo(user);
+			assertThat(session.getKeySet()).isEqualTo(unlockedKeySet);
+		}
+	}
+	
+	public static class Building_A_Session_For_Valid_Creds_With_An_Unxpected_Id extends Session_Context {
+		@Before
+		@Override
+		public void setup() throws Exception {
+			super.setup();
+		}
+		
+		@Test
+		public void itThrowsAn401() throws Exception {
+			try {
+				creds.buildSession(userDAO, "whee");
+				fail("should have thrown an auth challenge, but didn't");
+			} catch (WebApplicationException e) {
+				assertThat(e.getResponse().getStatus()).isEqualTo(Status.FORBIDDEN.getStatusCode());
+			}
 		}
 	}
 	
