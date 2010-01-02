09e069735008479a113a0240f269ccb1e2e64223
Coda Hale <coda.hale@gmail.com>
2010-01-01 23:41:58 -0800

    Changed UserResource to use Session.

--- src/main/java/com/wesabe/grendel/resources/UserResource.java
+++ src/main/java/com/wesabe/grendel/resources/UserResource.java
@@ -9,18 +9,17 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.core.Response.Status;
 
 import org.joda.time.DateTime;
 
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.wesabe.grendel.auth.Credentials;
+import com.wesabe.grendel.auth.Session;
 import com.wesabe.grendel.entities.Document;
 import com.wesabe.grendel.entities.User;
 import com.wesabe.grendel.entities.dao.UserDAO;
@@ -54,47 +53,44 @@ public class UserResource {
 	 */
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public UserInfoRepresentation show(@Context UriInfo uriInfo, @Context Credentials credentials, @PathParam("id") String id) {
-		final User user = findUser(id);
+	public UserInfoRepresentation show(@Context UriInfo uriInfo,
+		@Context Credentials credentials, @PathParam("id") String id) {
 		
-		if (isValidUsername(user, credentials.getUsername())) {
-			if (isValidPassword(user, credentials.getPassword())) {
-				
-				return new UserInfoRepresentation(uriInfo, user);
-			}
-		}
+		final Session session = credentials.buildSession(userDAO, id);
 		
-		throw new WebApplicationException(Credentials.CHALLENGE);
+		return new UserInfoRepresentation(uriInfo, session.getUser());
 	}
 	
 	/**
 	 * Responds to a {@link PUT} request by changing the user's password.
 	 * <p>
 	 * <strong>N.B.:</strong> Requires Basic authentication.
+	 * @throws CryptographicException
 	 */
 	@PUT
 	@Transactional
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response update(@Context Credentials credentials, @PathParam("id") String id, UpdateUserRepresentation request) {
+	public Response update(@Context Credentials credentials,
+		@PathParam("id") String id, UpdateUserRepresentation request) throws CryptographicException {
+		
 		request.validate();
 		
-		final User user = findUser(id);
+		final Session session = credentials.buildSession(userDAO, id);
+		final User user = session.getUser();
+		final UnlockedKeySet keySet = session.getKeySet();
 		
-		if (isValidUsername(user, credentials.getUsername())) {
-			try {
-				final UnlockedKeySet keySet = user.getKeySet().unlock(credentials.getPassword().toCharArray());
-				user.setKeySet(keySet.relock(credentials.getPassword().toCharArray(), request.getPassword(), randomProvider.get()));
-				
-				user.setModifiedAt(new DateTime());
-				userDAO.saveOrUpdate(user);
-				
-				return Response.noContent().build();
-			} catch (CryptographicException e) {
-				return Credentials.CHALLENGE;
-			}
-		}
+		user.setKeySet(
+			keySet.relock(
+				credentials.getPassword().toCharArray(),
+				request.getPassword(),
+				randomProvider.get()
+			)
+		);
 		
-		return Credentials.CHALLENGE;
+		user.setModifiedAt(new DateTime());
+		userDAO.saveOrUpdate(user);
+		
+		return Response.noContent().build();
 	}
 	
 	/**
@@ -105,38 +101,11 @@ public class UserResource {
 	 */
 	@DELETE
 	@Transactional
-	public Response delete(@Context UriInfo uriInfo, @Context Credentials credentials, @PathParam("id") String id) {
-		final User user = findUser(id);
+	public Response delete(@Context UriInfo uriInfo,
+		@Context Credentials credentials, @PathParam("id") String id) {
 		
-		if (isValidUsername(user, credentials.getUsername())) {
-			if (isValidPassword(user, credentials.getPassword())) {
-				userDAO.delete(user);
-				
-				return Response.noContent().build();
-			}
-		}
-		
-		return Credentials.CHALLENGE;
-	}
-
-	private User findUser(String id) {
-		final User user = userDAO.findById(id);
-		if (user == null) {
-			throw new WebApplicationException(Status.NOT_FOUND);
-		}
-		return user;
-	}
-	
-	private boolean isValidPassword(User user, String password) {
-		try {
-			user.getKeySet().unlock(password.toCharArray());
-			return true;
-		} catch (CryptographicException e) {
-			return false;
-		}
-	}
-
-	private boolean isValidUsername(User user, String username) {
-		return user.getId().equals(username);
+		final Session session = credentials.buildSession(userDAO, id);
+		userDAO.delete(session.getUser());
+		return Response.noContent().build();
 	}
 }
--- src/test/java/com/wesabe/grendel/resources/tests/UserResourceTest.java
+++ src/test/java/com/wesabe/grendel/resources/tests/UserResourceTest.java
@@ -1,12 +1,11 @@
 package com.wesabe.grendel.resources.tests;
 
 import static org.fest.assertions.Assertions.*;
-import static org.junit.Assert.*;
+import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.*;
 
 import java.security.SecureRandom;
 
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
@@ -18,15 +17,14 @@ import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InOrder;
-import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
 import com.google.inject.Provider;
 import com.wesabe.grendel.auth.Credentials;
+import com.wesabe.grendel.auth.Session;
 import com.wesabe.grendel.entities.User;
 import com.wesabe.grendel.entities.dao.UserDAO;
-import com.wesabe.grendel.openpgp.CryptographicException;
 import com.wesabe.grendel.openpgp.KeySet;
 import com.wesabe.grendel.openpgp.UnlockedKeySet;
 import com.wesabe.grendel.representations.UpdateUserRepresentation;
@@ -40,11 +38,16 @@ public class UserResourceTest {
 		protected UserDAO dao;
 		protected SecureRandom random;
 		protected Credentials credentials;
+		protected UnlockedKeySet keySet;
+		protected Session session;
 		protected UriInfo uriInfo;
 		protected User user;
 		
 		public void setup() throws Exception {
+			this.keySet = mock(UnlockedKeySet.class);
 			this.user = mock(User.class);
+			
+			this.session = new Session(user, keySet);
 			
 			this.uriInfo = mock(UriInfo.class);
 			when(uriInfo.getBaseUriBuilder()).thenAnswer(new Answer<UriBuilder>() {
@@ -54,9 +57,13 @@ public class UserResourceTest {
 				}
 			});
 			
-			this.credentials = new Credentials("bob", "secret");
+			
 			this.random = mock(SecureRandom.class);
 			this.dao = mock(UserDAO.class);
+			
+			this.credentials = mock(Credentials.class);
+			when(credentials.getPassword()).thenReturn("secret");
+			when(credentials.buildSession(dao, "bob")).thenReturn(session);
 			
 			this.resource = new UserResource(dao, new Provider<SecureRandom>() {
 				@Override
@@ -68,197 +75,65 @@ public class UserResourceTest {
 	}
 	
 	public static class Showing_A_User extends Context {
-		private KeySet keySet;
-		
 		@Before
 		@Override
 		public void setup() throws Exception {
 			super.setup();
-			
-			this.keySet = mock(KeySet.class);
-			when(user.getKeySet()).thenReturn(keySet);
-		}
-		
-		@Test
-		public void itReturnsA404IfTheUserDoesntExist() throws Exception {
-			when(dao.findById(Mockito.anyString())).thenReturn(null);
-			
-			try {
-				resource.show(uriInfo, credentials, "bob");
-				fail("should have thrown a 404 but didn't");
-			} catch (WebApplicationException e) {
-				assertThat(e.getResponse().getStatus()).isEqualTo(Status.NOT_FOUND.getStatusCode());
-			}
-		}
-		
-		@Test
-		public void itReturnsA401IfTheUsernameDoesntMatch() throws Exception {
-			when(dao.findById("bob")).thenReturn(user);
-			when(user.getId()).thenReturn("frank");
-			
-			try {
-				resource.show(uriInfo, credentials, "bob");
-				fail("should have thrown a 401 but didn't");
-			} catch (WebApplicationException e) {
-				assertThat(e.getResponse().getStatus()).isEqualTo(Status.UNAUTHORIZED.getStatusCode());
-			}
-		}
-		
-		@Test
-		public void itReturnsA401IfThePasswordDoesntMatch() throws Exception {
-			when(dao.findById("bob")).thenReturn(user);
-			when(user.getId()).thenReturn("bob");
-			
-			when(keySet.unlock(Mockito.any(char[].class))).thenThrow(new CryptographicException("whups"));
-			
-			try {
-				resource.show(uriInfo, credentials, "bob");
-				fail("should have thrown a 401 but didn't");
-			} catch (WebApplicationException e) {
-				assertThat(e.getResponse().getStatus()).isEqualTo(Status.UNAUTHORIZED.getStatusCode());
-			}
 		}
 		
 		@Test
 		public void itReturnsAUserListIfValid() throws Exception {
-			when(dao.findById("bob")).thenReturn(user);
-			when(user.getId()).thenReturn("bob");
-			
-			when(keySet.unlock(Mockito.any(char[].class))).thenReturn(null);
-			
 			final UserInfoRepresentation list = resource.show(uriInfo, credentials, "bob");
 			assertThat(list.getUser()).isEqualTo(user);
 			assertThat(list.getUriInfo()).isEqualTo(uriInfo);
-			
-			final ArgumentCaptor<char[]> captor = ArgumentCaptor.forClass(char[].class);
-			verify(keySet).unlock(captor.capture());
-			assertThat(captor.getValue()).isEqualTo("secret".toCharArray());
 		}
 	}
 	
 	public static class Deleting_A_User extends Context {
-		private KeySet keySet;
-		
 		@Before
 		@Override
 		public void setup() throws Exception {
 			super.setup();
-			
-			this.keySet = mock(KeySet.class);
-			when(user.getKeySet()).thenReturn(keySet);
 		}
-		
-		@Test
-		public void itReturnsA404IfTheUserDoesntExist() throws Exception {
-			when(dao.findById(Mockito.anyString())).thenReturn(null);
-			
-			try {
-				resource.delete(uriInfo, credentials, "bob");
-				fail("should have thrown a 404 but didn't");
-			} catch (WebApplicationException e) {
-				assertThat(e.getResponse().getStatus()).isEqualTo(Status.NOT_FOUND.getStatusCode());
-			}
-		}
-		
-		@Test
-		public void itReturnsA401IfTheUsernameDoesntMatch() throws Exception {
-			when(dao.findById("bob")).thenReturn(user);
-			when(user.getId()).thenReturn("frank");
-			
-			final Response response = resource.delete(uriInfo, credentials, "bob");
-			assertThat(response).isEqualTo(Credentials.CHALLENGE);
-		}
-		
-		@Test
-		public void itReturnsA401IfThePasswordDoesntMatch() throws Exception {
-			when(dao.findById("bob")).thenReturn(user);
-			when(user.getId()).thenReturn("bob");
-			when(keySet.unlock(Mockito.any(char[].class))).thenThrow(new CryptographicException("whups"));
-			
-			final Response response = resource.delete(uriInfo, credentials, "bob");
-			assertThat(response).isEqualTo(Credentials.CHALLENGE);
-		}
-		
+
 		@Test
 		public void itDeletesTheUserIfValid() throws Exception {
-			when(dao.findById("bob")).thenReturn(user);
-			when(user.getId()).thenReturn("bob");
-			
-			when(keySet.unlock(Mockito.any(char[].class))).thenReturn(null);
-			
 			final Response response = resource.delete(uriInfo, credentials, "bob");
 			assertThat(response.getStatus()).isEqualTo(Status.NO_CONTENT.getStatusCode());
-			
-			final ArgumentCaptor<char[]> captor = ArgumentCaptor.forClass(char[].class);
-			verify(keySet).unlock(captor.capture());
-			assertThat(captor.getValue()).isEqualTo("secret".toCharArray());
-			
+
 			verify(dao).delete(user);
 		}
 	}
-	
+
 	public static class Changing_A_Users_Password extends Context {
-		private KeySet keySet, newKeySet;
-		private UnlockedKeySet unlockedKeySet;
+		private KeySet newKeySet;
 		private UpdateUserRepresentation request;
-		
+
 		@Before
 		@Override
 		public void setup() throws Exception {
 			super.setup();
-			
-			this.keySet = mock(KeySet.class);
+
 			this.newKeySet = mock(KeySet.class);
-			this.unlockedKeySet = mock(UnlockedKeySet.class);
-			
-			when(keySet.unlock(Mockito.any(char[].class))).thenReturn(unlockedKeySet);
-			when(unlockedKeySet.relock(Mockito.any(char[].class), Mockito.any(char[].class), Mockito.any(SecureRandom.class))).thenReturn(newKeySet);
-			when(user.getKeySet()).thenReturn(keySet);
-			
+
+			when(keySet.relock(
+				any(char[].class), any(char[].class), any(SecureRandom.class))
+			).thenReturn(newKeySet);
+
 			this.request = new UpdateUserRepresentation();
 			request.setPassword("woohoo".toCharArray());
 		}
-		
+
 		@Test
-		public void itReturnsA404IfTheUserDoesntExist() throws Exception {
-			when(dao.findById(Mockito.anyString())).thenReturn(null);
-			
-			try {
-				resource.update(credentials, "bob", request);
-				fail("should have thrown a 404 but didn't");
-			} catch (WebApplicationException e) {
-				assertThat(e.getResponse().getStatus()).isEqualTo(Status.NOT_FOUND.getStatusCode());
-			}
-		}
-		
-		@Test
-		public void itReturnsA401IfTheUsernameDoesntMatch() throws Exception {
-			when(dao.findById("bob")).thenReturn(user);
-			when(user.getId()).thenReturn("frank");
-			
-			final Response response = resource.update(credentials, "bob", request);
-			assertThat(response).isEqualTo(Credentials.CHALLENGE);
-		}
-		
-		@Test
-		public void itChangesTheUsersPasswordIfValid() throws Exception {
-			when(dao.findById("bob")).thenReturn(user);
-			when(user.getId()).thenReturn("bob");
-			
-			
-			
+		public void itChangesTheUsersPassword() throws Exception {
 			final Response response = resource.update(credentials, "bob", request);
 			assertThat(response.getStatus()).isEqualTo(Status.NO_CONTENT.getStatusCode());
-			
-			ArgumentCaptor<char[]> captor = ArgumentCaptor.forClass(char[].class);
-			verify(keySet).unlock(captor.capture());
-			assertThat(captor.getValue()).isEqualTo("secret".toCharArray());
-			
-			captor = ArgumentCaptor.forClass(char[].class);
-			verify(unlockedKeySet).relock(captor.capture(), captor.capture(), Mockito.eq(random));
+
+			final ArgumentCaptor<char[]> captor = ArgumentCaptor.forClass(char[].class);
+			verify(keySet).relock(captor.capture(), captor.capture(), eq(random));
 			assertThat(captor.getAllValues().get(0)).isEqualTo("secret".toCharArray());
 			assertThat(captor.getAllValues().get(1)).isEqualTo("woohoo".toCharArray());
-			
+
 			final InOrder inOrder = inOrder(user, dao);
 			inOrder.verify(user).setKeySet(newKeySet);
 			inOrder.verify(dao).saveOrUpdate(user);
